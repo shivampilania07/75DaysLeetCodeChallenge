3e7df5ac6e38f86e7dd382dea519cd2b4f6e774c
Shivam Pilania <pilaniashivam25@gmail.com>
2026-03-12 09:30:26 +0530

    Time: 9 ms (19.19%), Space: 45.3 MB (48.09%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,21 +1,31 @@
 class Solution {
+    public int solve(int idx, int buy, int cap, int[] prices, int n, int[][][] dp) {
+        if (idx == n || cap == 0)
+            return 0;
+
+        if (dp[idx][buy][cap] != -1)
+            return dp[idx][buy][cap];
+
+        if (buy == 1) {
+            dp[idx][buy][cap] = Math.max(-prices[idx] + solve(idx + 1, 0, cap, prices, n, dp),
+                    solve(idx + 1, 1, cap, prices, n, dp));
+        } else {
+            dp[idx][buy][cap] = Math.max(prices[idx] + solve(idx + 1, 1, cap - 1, prices, n, dp),
+                    solve(idx + 1, 0, cap, prices, n, dp));
+        }
+
+        return dp[idx][buy][cap];
+    }
+
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n + 1][2][k+1];
-
-        for (int idx = n - 1; idx >= 0; idx--) {
-            for (int buy = 0; buy < 2; buy++) {
-                for (int cap = 1; cap <= k; cap++) {
-                    if (buy == 1) {
-                        dp[idx][buy][cap] = Math.max(-prices[idx] + dp[idx + 1][0][cap],
-                                dp[idx+1][1][cap]);
-                    } else {
-                        dp[idx][buy][cap] = Math.max(prices[idx] + dp[idx + 1][1][cap - 1],
-                                dp[idx + 1][ 0][cap]);
-                    }
-                }
+        int[][][] dp = new int[n][2][k+1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
             }
         }
-        return dp[0][1][k];
+
+        return solve(0, 1, k, prices, n, dp);
     }
 }
