20f05a76ffe971ff79cab658def0f1d8d0fd24be
Shivam Pilania <pilaniashivam25@gmail.com>
2026-03-29 10:11:00 +0530

    Time: 3 ms (87.35%), Space: 43.3 MB (66.55%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
+
+        for(int i =0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+
+                if((st.peek() == '(' && ch == ')') || (st.peek() == '{' && ch == '}') || (st.peek() == '[' && ch == ']')){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        if(st.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
