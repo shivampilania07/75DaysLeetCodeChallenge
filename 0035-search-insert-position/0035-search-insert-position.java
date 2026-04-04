8c40fadff9ac31e176872d13648fa33dff0593e6
Shivam Pilania <pilaniashivam25@gmail.com>
2026-04-04 15:17:41 +0530

    Time: 0 ms (100%), Space: 44.5 MB (87.47%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int i = 0;
+        int j = n-1;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                j = mid -1;
+            }else{
+                i = mid+1;
+            }
+        }
+        return i;
+    }
+}
