8b621bf2de233d35ef9a9d728d8df828ccd27e6c
Shivam Pilania <pilaniashivam25@gmail.com>
2026-04-29 09:18:04 +0530

    Time: 13 ms (14.4%), Space: 52.5 MB (11.22%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(board[i][j] != '.'){
+                    String b = "("+board[i][j]+")";
+                    if(!set.add(b+i) || !set.add(j+b) || !set.add(i/3+b+j/3)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
